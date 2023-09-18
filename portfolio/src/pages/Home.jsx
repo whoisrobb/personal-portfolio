09162c253e91ba48fcc1c5b3794e2376d9744f0f
Robbie Muchiri <developedbyrobbie@gmail.com>
2023-09-19 00:00:26 +0300

    minor design changes

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -2,34 +2,27 @@ import React, { useEffect } from 'react';
 import { gsap } from 'gsap';
 
 const Home = () => {
-  useEffect(() => {
-    const itemEls = document.querySelectorAll('.animate-item');
-
-    gsap.set(itemEls, { y: '20rem', opacity: 0 });
-
-    gsap.to(itemEls, {
-      y: 0,
-      opacity: 1,
-      duration: 0.9,
-      stagger: 0.1, // Stagger delay between each element
-    });
-  }, []);
 
   return (
     <section id='home' className='home'>
       <div className="wrapper">
-        <div className="name tabs">
-          <h1 className="animate-item">robert</h1>
-          <h1 className="animate-item">muchiri</h1>
+        <div className="one">
+
         </div>
-        <div className="intro">
-          <div className="tabs"><p className="animate-item">Full-stack developer based in Nairobi, Kenya.</p></div>
-          <div className="tabs"><p className="animate-item">I bring a versatile skill set in web development and a passion for crafting captivating digital experiences to the table.</p></div>
-          <div className="tabs"><p className="animate-item">I focus on creating dynamic front-end solutions that engage users as well as crafting optimal, integrated backend applications.</p></div>
+        <div className="two">
+          <p className="work">what I do</p>
+          <div className="desc">
+            <p>Crafting seamless web experiences through the fusion of aesthetics and functionality. I'm your fullstack web developer, open for opportunities.</p>
+          </div>
+        </div>
+        <div className="three">
+          <div className="intro">
+            <p>Hello World! I’m Robert Muchiri, a web enthusiast and my code weaves beautiful and functional web experiences</p>
+          </div>
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
 export default Home;
